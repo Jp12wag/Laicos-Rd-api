c865c0738fce6f40c3049dcf36ab267effcc291a
Wagner Alcantara <wagneralcantara36@gmail.com>
2024-09-13 09:27:32 -0400

    prueba

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-    const express = require('express');
-    const cors = require('cors');
-   // const memberRoutes = require('./routers/members');
-    const userRoutes = require('./routers/userRouter');
-    const connectDB = require('./db/conexion');
-    const app = express();
-    const PORT = process.env.PORT || 3001;
-    const corsOptions = {
-        origin: 'http://localhost:3000',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        allowedHeaders: 'Content-Type,Authorization'
-    };
-
-    connectDB();
+const express = require('express');
+const cors = require('cors');
+// const memberRoutes = require('./routers/members');
+const userRoutes = require('./routers/userRouter');
+const connectDB = require('./db/conexion');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type,Authorization'
+};
+connectDB();
 app.use(cors(corsOptions));
 app.use(express.json());
 
