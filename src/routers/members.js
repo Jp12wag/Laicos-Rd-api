64d258eaf4b84aa41ecfb75efb630aa0d674bb2e
Jose David <pereyrajosedavid@gmail.com>
2024-09-19 23:34:42 -0400

    Agregando controladores

--- src/routers/members.js
+++ src/routers/members.js
@@ -1,17 +1,17 @@
-const express = require('express');
-const Member = require('../models/Member');
+// const express = require('express');
+// const Member = require('../models/Member');
 
-const router = express.Router();
+// const router = express.Router();
 
-// Ruta para registrar un miembro
-router.post('/', async (req, res) => {
-  try {
-    const newMember = new Member(req.body);
-    await newMember.save();
-    res.status(201).json(newMember);
-  } catch (error) {
-    res.status(500).json({ message: 'Error al registrar miembro', error });
-  }
-});
+// // Ruta para registrar un miembro
+// router.post('/', async (req, res) => {
+//   try {
+//     const newMember = new Member(req.body);
+//     await newMember.save();
+//     res.status(201).json(newMember);
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error al registrar miembro', error });
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
