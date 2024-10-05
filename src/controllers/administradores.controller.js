054adff68a80429a08e01f46e5cb5bc3217e2abc
Wagner Alcantara <wagneralcantara36@gmail.com>
2024-10-05 13:21:19 -0400

    gestion de administrador

--- src/controllers/administradores.controller.js
+++ src/controllers/administradores.controller.js
@@ -7,39 +7,38 @@ const nodemailer = require('nodemailer');
 const controllers = {};
 
 controllers.createAdministrador = async (req, res) => {
-  const administrador = new Administrador(req.body);
 
-
-  const passSinAuth=administrador.password;
   try {
-      const secret = speakeasy.generateSecret();
-     
-      administrador.twoFactorSecret = secret.base32;
-     
-      await administrador.save();
-   
-      const otpauthUrl = secret.otpauth_url;
-      const qrCodeDataUrl = await qrcode.toDataURL(otpauthUrl);
-     
-      // Configuración del transportador de Nodemailer
-      const transporter = nodemailer.createTransport({
-        host: process.env.HOST,
-        port:2525,
-        auth: {
-          user: process.env.EMAIL_USER, // Tu correo de Hotmail
-          pass: process.env.EMAIL_PASS 
-        },
-        
-        logger: true, // Habilita el logging
-        debug: true // Muestra información adicional sobre el proceso
-      });
+    const administrador = new Administrador(req.body);
+    const passSinAuth = administrador.password;
+    const secret = speakeasy.generateSecret();
 
-      // Opciones del correo
-      const mailOptions = {
-          from: process.env.EMAIL_USER,
-          to: administrador.email, // El correo del usuario registrado
-          subject: 'Código QR y 2FA para tu cuenta',
-          html: `
+    administrador.twoFactorSecret = secret.base32;
+
+    await administrador.save();
+
+    const otpauthUrl = secret.otpauth_url;
+    const qrCodeDataUrl = await qrcode.toDataURL(otpauthUrl);
+
+    // Configuración del transportador de Nodemailer
+    const transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: 2525,
+      auth: {
+        user: process.env.EMAIL_USER, // Tu correo de Hotmail
+        pass: process.env.EMAIL_PASS
+      },
+
+      logger: true, // Habilita el logging
+      debug: true // Muestra información adicional sobre el proceso
+    });
+
+    // Opciones del correo
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: administrador.email, // El correo del usuario registrado
+      subject: 'Código QR y 2FA para tu cuenta',
+      html: `
               <p>Hola ${administrador.nombre},</p>
               <p>Gracias por registrarte. Aquí tienes tu código de autenticación en dos pasos y tu código QR:</p>
                <p><strong>su email: ${administrador.email} password: ${passSinAuth} </strong></p>
@@ -47,22 +46,22 @@ controllers.createAdministrador = async (req, res) => {
               <p>Escanea el siguiente código QR con tu aplicación de autenticación:</p>
               <p><img src=${qrCodeDataUrl} alt="Código QR de 2FA" /></p>
           `
-      };
+    };
 
-      // Enviar el correo electrónico
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              console.log('Error al enviar el correo:', error);
-              return res.status(500).send({ message: 'Error al enviar el correo.' });
-          }
-          console.log('Correo enviado: ' + info.response);
+    // Enviar el correo electrónico
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log('Error al enviar el correo:', error);
+        return res.status(500).send({ message: 'Error al enviar el correo.' });
+      }
+      console.log('Correo enviado: ' + info.response);
 
-          // Responder después de que el correo se haya enviado
-          return res.status(201).send({ administrador, qrcode: qrCodeDataUrl });
-      });
+      // Responder después de que el correo se haya enviado
+      return res.status(201).send({ administrador, qrcode: qrCodeDataUrl });
+    });
 
   } catch (e) {
-      res.status(400).send(e);
+    res.status(400).send(e);
   }
 };
 
@@ -89,29 +88,29 @@ controllers.getAdministradorById = async (req, res) => {
 
 controllers.loginAdministrador = async (req, res) => {
   try {
-      const administrador = await Administrador.findByCredentials(req.body.email, req.body.password);
-      
-      if (!administrador) {
-          return res.status(400).send({ error: 'Credenciales incorrectas' });
-      }
+    const administrador = await Administrador.findByCredentials(req.body.email, req.body.password);
 
-      // Verifica si el usuario tiene 2FA habilitado
-      if (administrador.twoFactorSecret) {
-          // Aquí se debe esperar el código 2FA del cliente
-          return res.status(200).send({ twoFactorRequired: true, administrador }); // Indica que se requiere 2FA
-      }
+    if (!administrador) {
+      return res.status(400).send({ error: 'Credenciales incorrectas' });
+    }
 
-      // Generar token si no se requiere 2FA
-      const token = await administrador.generateAuthToken();
-      res.status(200).send({ administrador, token, twoFactorRequired: false });
+    // Verifica si el usuario tiene 2FA habilitado
+    if (administrador.twoFactorSecret) {
+      // Aquí se debe esperar el código 2FA del cliente
+      return res.status(200).send({ twoFactorRequired: true, administrador }); // Indica que se requiere 2FA
+    }
+
+    // Generar token si no se requiere 2FA
+    const token = await administrador.generateAuthToken();
+    res.status(200).send({ administrador, token, twoFactorRequired: false });
 
   } catch (e) {
-      // Manejo de errores específico
-      if (e.message.includes('Invalid login credentials')) {
-          return res.status(401).send({ error: 'Credenciales inválidas' });
-      }
-      console.error(e); // Registrar el error para el desarrollo
-      res.status(500).send({ error: 'Ocurrió un error al iniciar sesión.' });
+    // Manejo de errores específico
+    if (e.message.includes('Invalid login credentials')) {
+      return res.status(401).send({ error: 'Credenciales inválidas' });
+    }
+    console.error(e); // Registrar el error para el desarrollo
+    res.status(500).send({ error: 'Ocurrió un error al iniciar sesión.' });
   }
 };
 
@@ -119,33 +118,33 @@ controllers.loginAdministrador = async (req, res) => {
 controllers.verifyTwoFactor = async (req, res) => {
   try {
     console.log(req.body.administradorId);
-      const administrador = await Administrador.findById(req.body.administradorId); // Asegúrate de pasar el administradorId correcto
-      const twoFactorCode = req.body.token; // Código 2FA ingresado por el usuario
+    const administrador = await Administrador.findById(req.body.administradorId); // Asegúrate de pasar el administradorId correcto
+    const twoFactorCode = req.body.token; // Código 2FA ingresado por el usuario
     console.log(twoFactorCode);
-      const isVerified = administrador.verifyTwoFactorToken(twoFactorCode);
-      console.log(isVerified);
-      if (!isVerified) {
-          return res.status(401).send({ error: 'Código 2FA inválido' });
-      }
+    const isVerified = administrador.verifyTwoFactorToken(twoFactorCode);
+    console.log(isVerified);
+    if (!isVerified) {
+      return res.status(401).send({ error: 'Código 2FA inválido' });
+    }
 
-      // Generar token después de verificar el 2FA
-      const token = await administrador.generateAuthToken();
-      res.status(200).send({ administrador, token });
+    // Generar token después de verificar el 2FA
+    const token = await administrador.generateAuthToken();
+    res.status(200).send({ administrador, token });
 
   } catch (e) {
-      console.error(e);
-      res.status(500).send({ error: 'Ocurrió un error al verificar el código 2FA.' });
+    console.error(e);
+    res.status(500).send({ error: 'Ocurrió un error al verificar el código 2FA.' });
   }
 };
 
 // Logout del usuario actual
 controllers.logoutadministrador = async (req, res) => {
   try {
-      req.administrador.tokens = req.administrador.tokens.filter((token) => token.token !== req.token);
-      await req.administrador.save();
-      res.send();
+    req.administrador.tokens = req.administrador.tokens.filter((token) => token.token !== req.token);
+    await req.administrador.save();
+    res.send();
   } catch (e) {
-      res.status(500).send(e);
+    res.status(500).send(e);
   }
 };
 
@@ -153,18 +152,18 @@ controllers.logoutadministrador = async (req, res) => {
 
 controllers.logoutAllAdministrador = async (req, res) => {
   try {
-      // Vacia todos los tokens del administrador
-      console.log('Antes:', req.administrador.tokens);
-      req.administrador.tokens = [];
-      
-      // Guarda los cambios en la base de datos
-      await req.administrador.save();
-      
-      // Envía una respuesta de éxito
-      res.send({ message: 'Sesión cerrada en todos los dispositivos.' });
+    // Vacia todos los tokens del administrador
+    console.log('Antes:', req.administrador.tokens);
+    req.administrador.tokens = [];
+
+    // Guarda los cambios en la base de datos
+    await req.administrador.save();
+
+    // Envía una respuesta de éxito
+    res.send({ message: 'Sesión cerrada en todos los dispositivos.' });
   } catch (e) {
-      // Si hay un error, devuelve una respuesta con el error
-      res.status(500).send(e);
+    // Si hay un error, devuelve una respuesta con el error
+    res.status(500).send(e);
   }
 };
 
@@ -197,26 +196,26 @@ controllers.requestResetPassword = async (req, res) => {
   const { email } = req.body;
   try {
     const administrador = await Administrador.findOne({ email });
-   
+
     if (!administrador) {
       return res.status(404).send({ error: 'Administrador no encontrado' });
     }
     // Generar un token para el restablecimiento de contraseña (puedes usar JWT o cualquier otro mecanismo)
-    const resetToken =  await administrador.generatePasswordResetToken();
+    const resetToken = await administrador.generatePasswordResetToken();
     //cambiar logica de para buscar el token 
     // Configuración del transportador de Nodemailer
     const transporter = nodemailer.createTransport({
       host: process.env.HOST,
-      port:2525,
+      port: 2525,
       auth: {
         user: process.env.EMAIL_USER, // Tu correo de Hotmail
-        pass: process.env.EMAIL_PASS 
+        pass: process.env.EMAIL_PASS
       },
-      
+
       logger: true, // Habilita el logging
       debug: true // Muestra información adicional sobre el proceso
     });
-//verificar variable globales
+    //verificar variable globales
 
 
     const mailOptions = {
@@ -227,7 +226,7 @@ controllers.requestResetPassword = async (req, res) => {
              <a href="http://localhost:3000/reset-password/${resetToken}">Restablecer contraseña</a>`
     };
 
-   
+
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         return console.log('Error al enviar correo:', error);
